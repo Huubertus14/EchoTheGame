6eee8caa13ea7dff2f84ba38674176f6ae391515
DESKTOP-Huub <huub_thomas@hotmail.com>
2023-01-06 21:12:40 +0100

    made some more work for the scoreboard. Got the hang now

--- EchoTheGame/Assets/Scripts/Player/Score/PlayerScoreboardController.cs
+++ EchoTheGame/Assets/Scripts/Player/Score/PlayerScoreboardController.cs
@@ -2,10 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Fusion;
+using Project.Echo.Setting;
 
 public class PlayerScoreboardController : NetworkBehaviour
 {
-    //When spawn set own name and send rpc to host
+	//When spawn set own name and send rpc to host
 
-    // if host do same but also receive rpc to clients to update score
+	// if host do same but also receive rpc to clients to update score
+	[SerializeField]private string _myName;
+	[SerializeField]private List<string> _playerNames;
+	private PlayerList _playerList;
+
+	public override void Spawned()
+	{
+		_playerList = GetComponentInChildren<PlayerList>();
+		_playerNames = new List<string>();
+		if (HasInputAuthority)
+		{
+			Debug.Log("Local player spawned");
+			RPC_SendNameToHost(Settings.Player.PlayerName);
+		}
+		else
+		{
+			Debug.Log("Not local player spawned");
+			_playerList.gameObject.SetActive(false);
+		}
+	}
+
+	[Rpc(sources: RpcSources.InputAuthority, targets: RpcTargets.StateAuthority)]
+	private void RPC_SendNameToHost(string name)
+	{
+		_myName = name;
+		Debug.Log($"New send name to host {name}");
+		_playerNames.Add(name);
+		RPC_ReceiveNewName(_myName);
+	}
+
+	[Rpc(sources: RpcSources.StateAuthority, targets: RpcTargets.All)]
+	private void RPC_ReceiveNewName(string newName) 
+	{
+		Debug.Log($"name received. Add to collection and send new list to clients {newName}");
+	}
+
+
 }
--- EchoTheGame/Assets/Scripts/UI/Game/PlayerList.cs
+++ EchoTheGame/Assets/Scripts/UI/Game/PlayerList.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class PlayerList : MonoBehaviour
 {
@@ -7,9 +9,36 @@ public class PlayerList : MonoBehaviour
 	[SerializeField]private PlayerListItem _listItemTemplate;
 	[SerializeField]private Canvas _canvas;
 
+	private List<PlayerListItem> _currentPlayers;
+
 	private void Awake()
 	{
+		_currentPlayers = new List<PlayerListItem>();
 		_canvas.worldCamera = Camera.main;
 		_listItemTemplate.gameObject.SetActive(false);
+	}
+
+	public void UpdateList(IReadOnlyList<string> nameCollection)
+	{
+		foreach (var sendName in nameCollection)
+		{
+			if (!_currentPlayers.Select(q=>q.PlayerName).Contains(sendName))
+			{
+				AddName(sendName);
+			}
+		}
+	}
+
+	public void AddName(string name)
+	{
+		PlayerListItem newItem = Instantiate(_listItemTemplate, transform);
+		newItem.Init(name);
+		newItem.gameObject.SetActive(true);
+		_currentPlayers.Add(newItem);
+	}
+
+	public void RemoveName(string nameToRemove)
+	{
+
 	}
 }
